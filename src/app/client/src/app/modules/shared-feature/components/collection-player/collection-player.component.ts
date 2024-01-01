91bbbede8a9d9774693d4d28facc34a38c90de58
Samarth Nigam Trigyn <137397906+samarthtrigyn@users.noreply.github.com>
2024-01-01 14:43:53 +0530

    removing unnecessary commented imports

--- src/app/client/src/app/modules/shared-feature/components/collection-player/collection-player.component.ts
+++ src/app/client/src/app/modules/shared-feature/components/collection-player/collection-player.component.ts
@@ -15,7 +15,6 @@ import * as TreeModel from 'tree-model';
 import { DeviceDetectorService } from 'ngx-device-detector';
 import { PopupControlService } from '../../../../service/popup-control.service';
 import { PublicPlayerService } from '@sunbird/public';
-// import { TocCardType, PlatformType } from '@dicdikshaorg/common-consumption';
 import { TocCardType, PlatformType } from '@dicdikshaorg/common-consumption';
 import { CsGroupAddableBloc } from '@project-sunbird/client-services/blocs';
 import { ContentManagerService } from '../../../public/module/offline/services';
