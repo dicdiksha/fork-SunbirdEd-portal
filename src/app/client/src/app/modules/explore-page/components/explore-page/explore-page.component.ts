6c1b38ff4efa4c2c7e731fd8eae13d9ae325b709
yashveertrigyn <yashveer.negi@trigyn.com>
2024-02-06 14:29:30 +0530

    117104- removed local storage code for global explore

--- src/app/client/src/app/modules/explore-page/components/explore-page/explore-page.component.ts
+++ src/app/client/src/app/modules/explore-page/components/explore-page/explore-page.component.ts
@@ -238,9 +238,10 @@ export class ExplorePageComponent implements OnInit, OnDestroy, AfterViewInit {
         } else {
             let guestUserDetails = JSON.parse(localStorage.getItem('guestUserDetails')) ?? {};
             if(guestUserDetails && Object.keys(guestUserDetails).length){
-                guestUserDetails.framework.board = ['CBSE'];
-                guestUserDetails.framework.id = 'ncert_k-12';
-                localStorage.setItem('guestUserDetails', JSON.stringify(guestUserDetails));
+                // guestUserDetails.framework.board = ['CBSE'];
+                // guestUserDetails.framework.id = 'ncert_k-12';
+                // localStorage.setItem('guestUserDetails', JSON.stringify(guestUserDetails));
+            } else {
                 this.router.navigateByUrl('/explore?board=CBSE/NCERT&gradeLevel=Class 1&gradeLevel=Class 2&&id=ncert_k-12&selectedTab=home');
             }
         }
