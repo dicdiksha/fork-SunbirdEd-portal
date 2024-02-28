5431d50aef3395634e908037bc9a3b948bea2645
Tejaswini Giram <tejaswini.giram@trigyn.com>
2024-02-28 19:33:18 +0530

    Added api to get token

--- src/app/client/src/app/modules/public/module/nishtha-dashboard/components/nishtha-dashboard.component.ts
+++ src/app/client/src/app/modules/public/module/nishtha-dashboard/components/nishtha-dashboard.component.ts
@@ -1,14 +1,15 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UtilService, ResourceService, LayoutService, NavigationHelperService, ToasterService, ConfigService, ContentUtilsServiceService } from '@sunbird/shared';
 import { CacheService } from 'ng2-cache-service';
-import { Subject } from 'rxjs';
-import { IInteractEventEdata, IImpressionEventInput, TelemetryService } from '@sunbird/telemetry';
-import { TenantService, PublicDataService } from '@sunbird/core';
+import { Observable, Subject, Subscription } from 'rxjs';
+import { IImpressionEventInput, TelemetryService } from '@sunbird/telemetry';
 import { Location } from '@angular/common';
 import { takeUntil } from 'rxjs/operators';
 import { DomSanitizer } from '@angular/platform-browser';
+import {  UserService } from '@sunbird/core'; 
+import {  IUserData } from '@sunbird/shared';
 @Component({
   selector: 'app-nishtha-dashboard',
   templateUrl: './nishtha-dashboard.component.html',
@@ -22,32 +23,64 @@ export class NishthaDashboardComponent implements OnInit {
   showLoader : boolean =false;
   siteUrl: string;
   sanatizedUrl: any;
+  userSubscription: Subscription;
+  userProfile:any;
+  tokenDetails: any;
   constructor(private http: HttpClient, private _cacheService: CacheService, private utilService: UtilService,
-    public tenantService: TenantService, public resourceService: ResourceService, public activatedRoute: ActivatedRoute,
+    public resourceService: ResourceService, public activatedRoute: ActivatedRoute, public userService: UserService,
     private layoutService: LayoutService, public navigationHelperService: NavigationHelperService, private location: Location,
     private router: Router, private telemetryService: TelemetryService,  public sanatizer: DomSanitizer) { }
 
   ngOnInit(): void {
     this.isDesktopApp = this.utilService.isDesktopApp;
+    this.userSubscription = this.userService.userData$.subscribe((user: IUserData) => {
+      if (user.userProfile) {
+        this.userProfile = user.userProfile;
+      }
+    });
     this.setTelemetryImpression();
     this.initLayout();
     this.openWebview();
+    console.log("this.this.userProfile--",this.userProfile);
+  }
+
+  getToken(): Observable<any>{
+    const URL = "https://diksha.gov.in/auth/realms/sunbird/protocol/openid-connect/token";
+  //   const headers = {
+  //     'Content-Type': 'application/x-www-form-urlencoded',
+  //   }
+  //  let params =  {
+  //     'client_id': 'ntp-support-tool',
+  //     'username': 'reportadmin@teamdiksha.org',
+  //     'grant_type': 'password',
+  //     'password': '',
+  //     'client_secret': '58d30df4-c5c7-48d4-906c-aa33b673e3c2'
+  //   }
+    
+    let headers: HttpHeaders = new HttpHeaders();
+    headers = headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    let input=`client_id=ntp-support-tool&username=${this.userProfile.userName}&grant_type=password&password=&client_secret=58d30df4-c5c7-48d4-906c-aa33b673e3c2`;
+    return this.http.post(URL,input,{headers:headers});
   }
 
   openWebview() {
-
     var generateUniqueId = '';
     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ-abcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
     for ( var i = 0; i < 20; i++ ) {
       generateUniqueId += characters.charAt(Math.floor(Math.random() * charactersLength));
-   }
-
-   this.sendInteractDataToTelemetry(generateUniqueId)
-    
-    this.siteUrl = 'https://nishthabi.akashic.dhira.io/login/?auth_token=eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJwdXFfSXEtQVZPM1psRENmN0wxOEp6M2xpSFRGMmhzeXl4dVkza1FfQm5BIn0.eyJleHAiOjE3MDg3NjI3MzcsImlhdCI6MTcwODc1OTE5NywianRpIjoiNjdkOWZmYzgtYjkzZi00YTM5LWJlYjUtZjhhM2I5NWU2OTdiIiwiaXNzIjoiaHR0cHM6Ly9rZXljbG9hay5ha2FzaGljLmRoaXJhLmlvL3JlYWxtcy9tYXN0ZXIiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiMGY4MGZjNzktNjlmZC00NTI0LThiZGEtNDJhNzJhNzljMzY5IiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiYWthc2hpYy1jb25zIiwic2Vzc2lvbl9zdGF0ZSI6ImVkZWNlMzM5LWE4ZTgtNGMxNS1hOGZlLWFlNjhjYzYzOTU2MiIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cDovL2FrYXNoaWMtdWktY29ucy5zMy13ZWJzaXRlLXVzLXdlc3QtMi5hbWF6b25hd3MuY29tIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJkZWZhdWx0LXJvbGVzLW1hc3RlciIsIm9mZmxpbmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwic2lkIjoiZWRlY2UzMzktYThlOC00YzE1LWE4ZmUtYWU2OGNjNjM5NTYyIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsIm5hbWUiOiJNYWhlc2ggYW5kdXJ0aGkiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJtYWhlc2guYW5kdXJ0aGlAZGhpcmEuaW8iLCJnaXZlbl9uYW1lIjoiTWFoZXNoIiwiZmFtaWx5X25hbWUiOiJhbmR1cnRoaSIsImVtYWlsIjoibWFoZXNoLmFuZHVydGhpQGRoaXJhLmlvIn0.r8ZzfJVLQAcZQcTBcKfV6VtaNCC3oS2wQoMyL1It3T23PL8Vz3r3e4YrLtHxHOh5fgKHIusj4wxCPb2urQgPkhbVOaqCuuU0Xfvm-ZytO7BkYgnwWwp-3mpMIa678RdI68xDhY97Iu6FrRdy0_e7Q0QmAOtxOXbmZ7lOtgN3jkZC3N3MWBDAEZ4ItQf3mgNiW2nMcaIt9ydHq1bWcjzle3SEWNh5ZEXRc1VuQjQ0eOVV8TglQ-n3q-1W3kW33kwCX2qXv0oDhpNmQVARj6HPP77WxXzGFJHZ3hPwFu6OMLyhfXa0l10Mz65NHZhkHAve8OHUAES_J2EkJrGmXHCc4A';
-    localStorage.setItem('nishthaDashboardUrl',this.siteUrl);
-   this.getUrl(this.siteUrl);
+    }
+    let token = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJhaE41YnpBQl95eDgzVTZBNlFZOFeyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJhaE41YnpBQl95eDgzVTZBNlFZOF9XcjFkUWpJSzJ2d0FvUmQ2M0Y2T05ZIn0.eyJqdGkiOiI1YjcxYjNhNy1hYWRkLTRmN2QtYjZiOS01ODBiNTY5OWUxOTgiLCJleHAiOjE3MDg4MTM1NTEsIm5iZiI6MCwiaWF0IjoxNzA4NzcwMzUxLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0L2F1dGgvcmVhbG1zL3N1bmJpcmQiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiNWY4OGFmNzYtMGJmZi00YWRiLTgzYzEtZTYwMDIwNTYzN2I4IiwidHlwIjoiQmVhcmVyIiwiYXpwIjoibmlzaHRoYS1kYXNoYm9hcmQiLCJhdXRoX3RpbWUiOjAsInNlc3Npb25fc3RhdGUiOiJhMjVlZDFlYy04MWJkLTQzYTEtYWIyMi1kZTc0MGUwNDdmMWMiLCJhY3IiOiIxIiwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6ImVtYWlsIHByb2ZpbGUiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsIm5hbWUiOiJNYWhlc2ggS3Jpc2huYWlhaCIsInByZWZlcnJlZF91c2VybmFtZSI6Im5pc2h0aGEtdXNlciIsImdpdmVuX25hbWUiOiJNYWhlc2giLCJmYW1pbHlfbmFtZSI6IktyaXNobmFpYWgiLCJlbWFpbCI6Im1haGVzaC5rcmlzaG5haWFoQHRyaWd5bi5jb20ifQ.YliXhd1Ns3-GRuWjsCTXEcQxuAgLHJFXPxoaMiNVPtkGDmAcZ84eSPZkRar3lpiNiSrr7Fp_rh2W7EvH0Xna8iAa7pVk2nEHvL2yihyI6DhuK8RzjfJK-_xZWC-adJtwwBdJApCw1tXBqKXrZH_B0tYSZ_V-AsYzajlNyFsSFpTYSLO4nJ8xEraDt3O6MqnHwKeyDRRnAF-M3x3soOV3gk_GDJaHcfKedZAok1GeqfJbEQ0YW_boaL4TShi1FH9TxR3t3LmAmdiImhBVdDcoaBfN_eXbS8IaaTvMn9Rg1LF3LUHAxlq-k3w-_gjs0A-l_TVVnlELw1axm8jQPRCMPg';
+    if(this.isUserLoggedIn) {
+      this.getToken().subscribe(res=>{
+        if(res) {
+          token = res.access_token;
+        }
+      });
+    }
+    this.sendInteractDataToTelemetry(generateUniqueId);    
+    this.siteUrl = 'https://nishtha-dashboard.diksha.gov.in/login/?diksha_token='+token;
+    this.getUrl(this.siteUrl);
   }
 
   getUrl(url: any) {
@@ -97,6 +130,10 @@ export class NishthaDashboardComponent implements OnInit {
 
   goBack() {
     this.location.back();
+  }
+
+  public isUserLoggedIn(): boolean {
+    return this.userService && (this.userService.loggedIn || false);
   }
 
 }
