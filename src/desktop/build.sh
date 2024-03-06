b667efb5ee0d83a8e4e8347c65a088a603865260
sagar-trigyn <sagar.singh@trigyn.com>
2024-03-06 17:50:28 +0530

    change in build.sh

--- src/desktop/build.sh
+++ src/desktop/build.sh
@@ -4,20 +4,22 @@ rm -rf app_dist
 cd ../app/client
 
 npm install --ignore-engines
+npm install @angular/cli@9.0.4
 npm run prod-desktop
+npm install @angular/cli@9.0.4
 npm install --save @types/lodash@4.14.74
 cd ..
 npm install
 npm run  resource-bundles
-npm install @angular/cli@9.0.4
 cd ../desktop/OpenRAP
-npm i @angular/cli@9.0.4
 npm install
 npm run pack 
 
 cd ..
 npm --update-checksums
+npm install fs-extra
 npm install
+npm install fs-extra
 npm run build-ts
 node scripts/copy.js
 npm install fs-extra
