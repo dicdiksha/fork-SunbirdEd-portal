74de9e53c26f5f87e705191d2ce8919ec49d3cc9
sagar-trigyn <sagar.singh@trigyn.com>
2024-03-11 11:44:53 +0530

    chnage in bui

--- src/desktop/build.sh
+++ src/desktop/build.sh
@@ -2,27 +2,19 @@
 cd /offline/src/desktop
 rm -rf app_dist
 cd ../app/client
-
-npm install --ignore-engines
-npm install @angular/cli@9.0.4
+yarn install
 npm run prod-desktop
-npm install @angular/cli@9.0.4
-npm install --save @types/lodash@4.14.74
 cd ..
-npm install
+yarn install
 npm run  resource-bundles
 cd ../desktop/OpenRAP
-npm install
-npm run pack 
-
+yarn install
+npm run pack
 cd ..
-npm --update-checksums
-npm install fs-extra
-npm install
-npm install fs-extra
+yarn --update-checksums
+yarn install
 npm run build-ts
 node scripts/copy.js
-npm install fs-extra
 
 
 # Tar the generic build files
