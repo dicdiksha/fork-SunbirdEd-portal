7e5a9a61b468b4d71a834ec9c9ab6312febc2f6e
sagar-trigyn <sagar.singh@trigyn.com>
2024-03-11 13:34:06 +0530

    added angular version and type@yargs

--- src/desktop/build.sh
+++ src/desktop/build.sh
@@ -2,20 +2,21 @@
 cd /offline/src/desktop
 rm -rf app_dist
 cd ../app/client
-yarn install
+yarn install --ignore-engines
+npm install -g @angular/cli
 npm run prod-desktop
 cd ..
-yarn install
+yarn install --ignore-engines
 npm run  resource-bundles
 cd ../desktop/OpenRAP
-yarn install
+yarn install --ignore-engines
 npm run pack
 cd ..
 yarn --update-checksums
-yarn install
+yarn install --ignore-engines
 npm run build-ts
+npm install --save @types/yargs
 node scripts/copy.js
-
 
 # Tar the generic build files
 tar -czvf app_dist.tar.gz app_dist
