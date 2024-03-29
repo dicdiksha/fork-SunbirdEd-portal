0cd6ae84af080b47f1e5dda84556975837399927
yashveertrigyn <yashveer.negi@trigyn.com>
2024-03-29 22:57:57 +0530

    143051- Profile update bug fix for subject

--- src/app/client/src/app/modules/shared-feature/components/profile-framework-popup/profile-framework-popup.component.ts
+++ src/app/client/src/app/modules/shared-feature/components/profile-framework-popup/profile-framework-popup.component.ts
@@ -300,11 +300,11 @@ export class ProfileFrameworkPopupComponent implements OnInit, OnDestroy {
     return this.formService.getFormConfig(formServiceInputParams, hashTagId);
   }
   onSubmitForm() {
-    const selectedOption = _.cloneDeep(this.selectedOption);
+    let selectedOption = _.cloneDeep(this.selectedOption);
     selectedOption.board = _.get(this.selectedOption, 'board') ? [this.selectedOption.board] : [];
     selectedOption.medium = _.get(this.selectedOption, 'medium') ? [this.selectedOption.medium] : [];
     selectedOption.gradeLevel = _.get(this.selectedOption, 'gradeLevel') ? [this.selectedOption.gradeLevel] : [];
-    selectedOption.subject = _.get(this.selectedOption, 'subject') ? [this.selectedOption.subject] : [];
+    selectedOption.subject = _.get(this.selectedOption, 'subject') ? this.selectedOption.subject : [];
     selectedOption.id = this.frameWorkId;
     if (selectedOption.board) {
       _.forEach(selectedOption.board, (data, index) => {
