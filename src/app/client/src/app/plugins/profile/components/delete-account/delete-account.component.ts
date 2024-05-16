1813bc1eb9f49fa20334ac7920eb0227dab32870
yashveertrigyn <yashveer.negi@trigyn.com>
2024-05-16 15:05:58 +0530

    uncomment otp html

--- src/app/client/src/app/plugins/profile/components/delete-account/delete-account.component.ts
+++ src/app/client/src/app/plugins/profile/components/delete-account/delete-account.component.ts
@@ -121,28 +121,28 @@ export class DeleteAccountComponent implements OnInit, OnDestroy {
     );
   }
 
-  // verificationSuccess(data) {
-  //   this.userService.deleteUser().subscribe(
-  //     (data: ServerResponse) => {
-  //       if(_.get(data, 'result.response') === 'SUCCESS'){
-  //         window.location.replace('/logoff');
-  //         this.cacheService.removeAll();
-  //         if(this.deviceDetectorService.isMobile()){
-  //           //TODO changes need to be done on the Mobile Deeplink
-  //           const url ='dev.sunbird.app://mobile?userId'+ this.userProfile.userId;
-  //           window.open(url, '_blank');
-  //         }
-  //         window.location.replace('/logoff');
-  //         this.cacheService.removeAll();
-  //       }
-  //     },
-  //     (err) => {
-  //       //TODO we need to update the error 
-  //       const errorMessage =  this.resourceService.messages.fmsg.m0085;
-  //       this.toasterService.error(errorMessage);
-  //     }
-  //   );
-  // }
+  verificationSuccess(data) {
+    // this.userService.deleteUser().subscribe(
+    //   (data: ServerResponse) => {
+    //     if(_.get(data, 'result.response') === 'SUCCESS'){
+    //       window.location.replace('/logoff');
+    //       this.cacheService.removeAll();
+    //       if(this.deviceDetectorService.isMobile()){
+    //         //TODO changes need to be done on the Mobile Deeplink
+    //         const url ='dev.sunbird.app://mobile?userId'+ this.userProfile.userId;
+    //         window.open(url, '_blank');
+    //       }
+    //       window.location.replace('/logoff');
+    //       this.cacheService.removeAll();
+    //     }
+    //   },
+    //   (err) => {
+    //     //TODO we need to update the error 
+    //     const errorMessage =  this.resourceService.messages.fmsg.m0085;
+    //     this.toasterService.error(errorMessage);
+    //   }
+    // );
+  }
 
   setInteractEventData() {
     const id = 'delete-account';
