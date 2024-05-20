fb0f56aad582bd3fb258a01e133c8231c05af4d2
Satyanarayana Khaja <satyanarayana.khaja@trigyn.com>
2024-05-20 19:26:59 +0530

    modified certificate changes

--- src/app/client/src/app/modules/shared/directives/certificates/certificate-download-as-pdf.service.ts
+++ src/app/client/src/app/modules/shared/directives/certificates/certificate-download-as-pdf.service.ts
@@ -1,5 +1,5 @@
 import {Inject, Injectable} from '@angular/core';
-import {CertificateDirectivesUtility} from './certificate-directives-utility';
+import {CertificateDirectivesUtility} from '../certificates/certificate-directives-utility';
 
 @Injectable()
 export class CertificateDownloadAsPdfService {
@@ -24,24 +24,51 @@ export class CertificateDownloadAsPdfService {
 
     canvasElement.innerHTML = template;
 
-    const domtoimage = await this.domtoimageModule;
+    // const domtoimage = await this.domtoimageModule;
 
-    domtoimage.toPng(canvasElement, {
-      style: {
-        left: '0',
-        right: '0',
-        bottom: '0',
-        top: '0'
-      }
-    })
-    .then(async (blob) => {
-      const JsPDF = await this.jsPDFModule;
+    // domtoimage.toPng(canvasElement, {
+    //   style: {
+    //     left: '0',
+    //     right: '0',
+    //     bottom: '0',
+    //     top: '0'
+    //   }
+    // })
+    // .then(async (blob) => {
+    //   const JsPDF = await this.jsPDFModule;
+    //   const pdf = new JsPDF({
+    //     orientation: 'landscape',
+    //     unit: 'pt',
+    //     format: [
+    //       1060 / 1.33, 750 / 1.33
+    //     ]
+    //   });
+    const svgToPng = new Promise<Blob>((resolve, reject) => {
+      const svgString = new XMLSerializer().serializeToString(canvasElement.firstChild);
+      const img = new Image();
+      img.onload = () => {
+        const canvas = document.createElement('canvas');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0);
+        canvas.toBlob((blob: Blob) => {
+          if (blob) {
+            resolve(blob);
+          } else {
+            reject('Error converting SVG to PNG');
+          }
+        });
+      };
+      img.src = 'data:image/svg+xml;base64,' + btoa(unescape(encodeURIComponent(svgString)));
+    });
+
+    svgToPng.then(async (blob) => {
+        const JsPDF = await this.jsPDFModule;
       const pdf = new JsPDF({
         orientation: 'landscape',
         unit: 'pt',
-        format: [
-          1060 / 1.33, 750 / 1.33
-        ]
+        format: [1060 / 1.33, 750 / 1.33]
       });
       pdf.addImage(blob, 'PNG', 0, 0);
 
