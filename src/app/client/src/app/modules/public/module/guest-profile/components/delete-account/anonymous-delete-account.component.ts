555d0d426e0658dd030f8d5867c5b73bd5fa7c91
yashveertrigyn <yashveer.negi@trigyn.com>
2024-05-22 12:36:39 +0530

    OTPTemplate.userDeleteTemplate

--- src/app/client/src/app/modules/public/module/guest-profile/components/delete-account/anonymous-delete-account.component.ts
+++ src/app/client/src/app/modules/public/module/guest-profile/components/delete-account/anonymous-delete-account.component.ts
@@ -29,7 +29,7 @@ export class AnonymousDeleteAccountComponent implements OnInit, OnDestroy {
   submitInteractEdata: IInteractEventEdata;
   telemetryInteractObject: IInteractEventObject;
   verifiedUser = false;
-  templateId: any = 'otpContactUpdateTemplate';
+  templateId: any = 'deleteUserAccountTemplate';
 
   constructor(public resourceService: ResourceService,
     public otpService: OtpService, public toasterService: ToasterService,
@@ -46,8 +46,8 @@ export class AnonymousDeleteAccountComponent implements OnInit, OnDestroy {
       const request: any = {
         key: this.contactType,
         userId: this.userId,
-        //templateId: this.configService.appConfig.OTPTemplate.userDeleteTemplate,
-        templateId: 'otpContactUpdateTemplate',
+        templateId: this.configService.appConfig.OTPTemplate.userDeleteTemplate,
+        // templateId: 'otpContactUpdateTemplate',
         type: ''
       };
       if ((this.contactType === 'email') || this.contactType === 'recoveryEmail') {
