42bafd68ed229e6b82c5f89afde60a1a09e620de
Tejaswini Giram <tejaswini.giram@trigyn.com>
2024-06-04 18:52:34 +0530

    161855 - Added token along with updated nishtha-dashboard url

--- src/app/client/src/app/modules/public/module/nishtha-dashboard/components/nishtha-dashboard.component.ts
+++ src/app/client/src/app/modules/public/module/nishtha-dashboard/components/nishtha-dashboard.component.ts
@@ -48,7 +48,6 @@ export class NishthaDashboardComponent implements OnInit {
     let headers: HttpHeaders = new HttpHeaders();
     headers = headers.append('Content-Type', 'application/x-www-form-urlencoded');
     let input=`client_id=ntp-support-tool&username=reportadmin@teamdiksha.org&grant_type=password&password=&client_secret=58d30df4-c5c7-48d4-906c-aa33b673e3c2`;
-    console.log("input--->",input);
     return this.http.post(URL,input,{headers:headers});
   }
 
@@ -60,16 +59,16 @@ export class NishthaDashboardComponent implements OnInit {
       generateUniqueId += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     let token = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJhaE41YnpBQl95eDgzVTZBNlFZOFeyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJhaE41YnpBQl95eDgzVTZBNlFZOF9XcjFkUWpJSzJ2d0FvUmQ2M0Y2T05ZIn0.eyJqdGkiOiI1YjcxYjNhNy1hYWRkLTRmN2QtYjZiOS01ODBiNTY5OWUxOTgiLCJleHAiOjE3MDg4MTM1NTEsIm5iZiI6MCwiaWF0IjoxNzA4NzcwMzUxLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0L2F1dGgvcmVhbG1zL3N1bmJpcmQiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiNWY4OGFmNzYtMGJmZi00YWRiLTgzYzEtZTYwMDIwNTYzN2I4IiwidHlwIjoiQmVhcmVyIiwiYXpwIjoibmlzaHRoYS1kYXNoYm9hcmQiLCJhdXRoX3RpbWUiOjAsInNlc3Npb25fc3RhdGUiOiJhMjVlZDFlYy04MWJkLTQzYTEtYWIyMi1kZTc0MGUwNDdmMWMiLCJhY3IiOiIxIiwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6ImVtYWlsIHByb2ZpbGUiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsIm5hbWUiOiJNYWhlc2ggS3Jpc2huYWlhaCIsInByZWZlcnJlZF91c2VybmFtZSI6Im5pc2h0aGEtdXNlciIsImdpdmVuX25hbWUiOiJNYWhlc2giLCJmYW1pbHlfbmFtZSI6IktyaXNobmFpYWgiLCJlbWFpbCI6Im1haGVzaC5rcmlzaG5haWFoQHRyaWd5bi5jb20ifQ.YliXhd1Ns3-GRuWjsCTXEcQxuAgLHJFXPxoaMiNVPtkGDmAcZ84eSPZkRar3lpiNiSrr7Fp_rh2W7EvH0Xna8iAa7pVk2nEHvL2yihyI6DhuK8RzjfJK-_xZWC-adJtwwBdJApCw1tXBqKXrZH_B0tYSZ_V-AsYzajlNyFsSFpTYSLO4nJ8xEraDt3O6MqnHwKeyDRRnAF-M3x3soOV3gk_GDJaHcfKedZAok1GeqfJbEQ0YW_boaL4TShi1FH9TxR3t3LmAmdiImhBVdDcoaBfN_eXbS8IaaTvMn9Rg1LF3LUHAxlq-k3w-_gjs0A-l_TVVnlELw1axm8jQPRCMPg';
-    // if(this.isUserLoggedIn) {
-    //   this.getToken().subscribe(res=>{
-    //     console.log("res--->",res);
-    //     if(res) {
-    //       token = res.access_token;
-    //     }
-    //   });
-    // }
+    if(this.isUserLoggedIn) {
+      this.getToken().subscribe(res=>{
+        console.log("res--->",res);
+        if(res) {
+          token = res.access_token;
+        }
+      });
+    }
     this.sendInteractDataToTelemetry(generateUniqueId);    
-    this.siteUrl = 'https://course-data.diksha.gov.in';
+    this.siteUrl = 'https://course-data.diksha.gov.in/login/?diksha_token='+token;
     this.getUrl(this.siteUrl);
   }
 
