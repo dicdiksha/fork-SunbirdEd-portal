67f1a9c26b75257b3ea5b8a65c320038fe6af436
Tejaswini Giram <tejaswini.giram@trigyn.com>
2024-06-04 20:30:22 +0530

    160452 - Fixed issue when we clearing deatils while updating batch

--- src/app/client/src/app/modules/learn/components/batch/update-course-batch/update-course-batch.component.ts
+++ src/app/client/src/app/modules/learn/components/batch/update-course-batch/update-course-batch.component.ts
@@ -245,15 +245,15 @@ export class UpdateCourseBatchComponent implements OnInit, OnDestroy, AfterViewI
     }
   }
   private isSubmitBtnDisable(batchForm): boolean {
-    const batchFormControls = ['name', 'description', 'enrollmentType', 'mentors', 'startDate', 'endDate', 'users'];
+    const batchFormControls = ['name', 'description', 'enrollmentType', 'mentors', 'startDate', 'endDate', 'users', 'enrollmentEndDate'];
     for (let i = 0; i < batchFormControls.length; i++) {
       if (batchForm.controls[batchFormControls[i]].status !== 'VALID') {
         return true;
       }
     }
-    if (batchForm.controls['enrollmentEndDate'].status !== 'VALID' && batchForm.controls['enrollmentEndDate'].pristine) {
-      return false;
-    }
+    // if (batchForm.controls['enrollmentEndDate'].status !== 'VALID' && batchForm.controls['enrollmentEndDate'].pristine) {
+    //   return false;
+    // }
     return true;
   }
   /**
