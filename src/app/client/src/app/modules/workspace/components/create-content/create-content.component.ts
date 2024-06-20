85afc239e97c44a41fbccf79cf92cec2a8a513aa
Purushotam <purushotam.kumar@trigyn.com>
2024-06-20 16:07:42 +0530

    LMS SSO integration for Creator and Reviewer account

--- src/app/client/src/app/modules/workspace/components/create-content/create-content.component.ts
+++ src/app/client/src/app/modules/workspace/components/create-content/create-content.component.ts
@@ -1,9 +1,13 @@
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit, AfterViewInit } from '@angular/core';
-import { ResourceService, ConfigService, NavigationHelperService } from '@sunbird/shared';
+import { ResourceService, ConfigService, NavigationHelperService, ServerResponse } from '@sunbird/shared';
 import { FrameworkService, PermissionService, UserService } from '@sunbird/core';
 import { IImpressionEventInput } from '@sunbird/telemetry';
 import { WorkSpaceService } from './../../services';
+import { LearnerService } from '@sunbird/core';
+import {
+  userLMSToken
+ } from '../../../core/services/userTokenForLMS/userLMSToken';
 @Component({
   selector: 'app-create-content',
   templateUrl: './create-content.component.html'
@@ -67,11 +71,19 @@ export class CreateContentComponent implements OnInit, AfterViewInit {
 
   * @param {ResourceService} resourceService Reference of ResourceService
  */
+
+  userProfile: any;
+  userData:any;
+
   constructor(configService: ConfigService, resourceService: ResourceService,
     frameworkService: FrameworkService, permissionService: PermissionService,
     private activatedRoute: ActivatedRoute, public userService: UserService,
     public navigationhelperService: NavigationHelperService,
-    public workSpaceService: WorkSpaceService) {
+    public workSpaceService: WorkSpaceService,
+    public config: ConfigService,
+    public learnerService: LearnerService,
+    private userLMSToken: userLMSToken,
+  ) {
     this.resourceService = resourceService;
     this.frameworkService = frameworkService;
     this.permissionService = permissionService;
@@ -111,4 +123,77 @@ export class CreateContentComponent implements OnInit, AfterViewInit {
       };
     });
   }
+
+
+  navigateToLMSWeb() {
+    const optionData = {
+        url: `${this.config.urlConFig.URLS.USER.GET_PROFILE}${this.userProfile.userId}${'?userdelete=true'}`, // userdelete is not actual deleted user data this is basically unmaksed phone no. & email id and give us reponse
+        //   param: this.config.urlConFig.params.userReadParam
+    };
+
+    this.learnerService.getWithHeaders(optionData).subscribe(
+        (data: ServerResponse) => {
+            if (data?.result && (data?.result?.response?.phone || data?.result?.response?.email)) {
+
+                console.log("user data.....", data?.result?.response);
+                let ids = []; // locations ids -> state, district,block , cluster, school
+
+                data?.result?.response?.profileLocation?.forEach((element: any) => {
+                    ids.push(element?.id)
+                });
+
+                console.log("IDS.......", ids);
+
+                if (ids?.length) {
+                    this.userLMSToken.getUserLocationData(ids)
+                        .then(locationData => {
+                            console.log("locationData?.result?.response", locationData?.result?.response)
+                            this.userData = locationData;
+                            console.log("this?.userData", this.userData);
+
+                            const createLocationObject = (locations: any) => {
+                                return locations?.reduce((acc: any, location: any) => {
+                                    acc[location.type] = location.name;
+                                    if (location.type === 'school') {
+                                        acc.code = location.code;
+                                    }
+                                    return acc;
+                                }, {});
+                            };
+
+                            const locationObject = createLocationObject(this?.userData?.result?.response);
+                            console.log("locationObject", locationObject);
+
+                            const userDataObject = {
+                                firstname: data?.result?.response?.firstName,
+                                lastname: data?.result?.response?.lastName,
+                                emailid: data?.result?.response?.email,
+                                phone: data?.result?.response?.phone,
+                                userid: data?.result?.response?.userId,
+                                profileUserType: data?.result?.response?.profileUserType?.type,
+                                profileUserSubType: data?.result?.response?.profileUserSubType?.subType,
+                                rootOrgName: data?.result?.response?.rootOrg?.description,
+                                board: data?.result?.response?.framework?.board[0] ? data?.result?.response?.framework?.board[0] : null,
+                                ...locationObject, // keys name {state, district, block, cluster, school, code}
+                            }
+
+                            console.log("final object", userDataObject);
+                            const apiUrl = 'https://jenkins.oci.diksha.gov.in/diksha-jwttoken/jwtlmsgenarator';
+                            const url = `${apiUrl}?userid=${userDataObject?.userid}&firstname=${userDataObject?.firstname}&lastname=${userDataObject?.lastname}&emailid=${userDataObject?.emailid}&phone=${userDataObject?.phone}&profileUserType=${userDataObject?.profileUserType}&board=${userDataObject?.board}&state=${userDataObject?.state}&district=${userDataObject?.district}&block=${userDataObject?.block}&cluster=${userDataObject?.cluster}&school=${userDataObject?.school}&code=${userDataObject?.code}&rootOrgName=${userDataObject?.rootOrgName}&profileUserSubType=${userDataObject?.profileUserSubType}`;
+                            // window.location.href = url; // open in same tab
+                            window.open(url, '_blank'); // open in new tab
+                        })
+                        .catch(error => {
+                            console.error(error);
+                        });
+                }
+
+            }
+        },
+        (err: ServerResponse) => {
+            console.log("getDecriptedUserProfile error ", err);
+        }
+    )
+}
+
 }
