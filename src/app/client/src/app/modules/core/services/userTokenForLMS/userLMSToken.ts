c2b594df817e269f222d0427ead664bbd2185c2c
Purushotam <purushotam.kumar@trigyn.com>
2024-06-21 17:20:18 +0530

    added common method to get data for LMS

--- src/app/client/src/app/modules/core/services/userTokenForLMS/userLMSToken.ts
+++ src/app/client/src/app/modules/core/services/userTokenForLMS/userLMSToken.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+import { UserService, LearnerService } from '@sunbird/core';
+import { ConfigService, ServerResponse } from '@sunbird/shared';
 @Injectable({
     providedIn: 'root'
 })
 export class userLMSToken {
 
     private apiUrl = 'https://jenkins.oci.diksha.gov.in/diksha-jwttoken/jwtlmsgenarator';
-    private uerLocationUrl = 'https://dev.oci.diksha.gov.in/learner/user/v5/read/2f47892e-0de1-409e-8b05-4f5d9db18278?userdelete=true&fields=organisations,roles,locations,declarations,externalIds';
+
+    constructor(private http: HttpClient,
+        public userService: UserService,
+        public config: ConfigService,
+        public learnerService: LearnerService,
+    ) { }
 
 
-    constructor(private http: HttpClient) { }
 
     getToken(payload: any): Promise<any> {
         const { userid, firstname, lastname, emailid, phone } = payload;
@@ -40,5 +45,81 @@ export class userLMSToken {
 
         return this.http.post(apiUrl, data, { headers }).toPromise();
     }
+
+
+   
+    // LMS part start
+    public isUserLoggedIn(): boolean {
+        return this.userService && (this.userService.loggedIn || false);
+    }
+    userData: any;
+    lmsData: any;
+
+    /**
+      * this is common method to get data form LMS - 
+      * it can use for LMS requirement to redirect from DIKSHA to LMS web portal
+     */
+    getDataForLMS(): Promise<any> {
+        console.log("getting user id in common method....", this.userService?.userProfile?.userId);
+        console.log("isUserLoggedIn in common method", this.isUserLoggedIn());
+        const optionData = {
+            url: `${this.config.urlConFig.URLS.USER.GET_PROFILE}${this.userService?.userProfile?.userId}${'?userdelete=true'}`, // userdelete is not actual deleted user data this is basically unmaksed phone no. & email id and give us reponse
+            //   param: this.config.urlConFig.params.userReadParam
+        };
+        this.learnerService.getWithHeaders(optionData).subscribe(
+            (data: ServerResponse) => {
+                if (data?.result && (data?.result?.response?.phone || data?.result?.response?.email)) {
+                    console.log("user data.....", data?.result?.response);
+                    let ids = []; // locations ids -> state, district,block , cluster, school
+                    data?.result?.response?.profileLocation?.forEach((element: any) => {
+                        ids.push(element?.id)
+                    });
+                    console.log("IDS.......", ids);
+                    if (ids?.length && this.isUserLoggedIn()) {
+                        this.getUserLocationData(ids)
+                            .then(locationData => {
+                                console.log("locationData?.result?.response", locationData?.result?.response)
+                                this.userData = locationData;
+                                console.log("this?.userData", this.userData);
+                                const createLocationObject = (locations: any) => {
+                                    return locations?.reduce((acc: any, location: any) => {
+                                        acc[location.type] = location.name;
+                                        if (location.type === 'school') {
+                                            acc.code = location.code;
+                                        }
+                                        return acc;
+                                    }, {});
+                                };
+                                const locationObject = createLocationObject(this?.userData?.result?.response);
+                                console.log("locationObject", locationObject);
+                                const userDataObject = {
+                                    firstname: data?.result?.response?.firstName,
+                                    lastname: data?.result?.response?.lastName,
+                                    emailid: data?.result?.response?.email,
+                                    phone: data?.result?.response?.phone,
+                                    userid: data?.result?.response?.userId,
+                                    profileUserType: data?.result?.response?.profileUserType?.type,
+                                    profileUserSubType: data?.result?.response?.profileUserSubType?.subType,
+                                    rootOrgName: data?.result?.response?.rootOrg?.description,
+                                    board: data?.result?.response?.framework?.board[0] ? data?.result?.response?.framework?.board[0] : null,
+                                    ...locationObject, // keys name {state, district, block, cluster, school, code}
+                                }
+                                // return userDataObject;
+                                console.log("userDataObject....", userDataObject)
+                                this.lmsData = userDataObject;
+                            })
+                            .catch(error => {
+                                console.error(error);
+                            });
+                    }
+                }
+            },
+            (err: ServerResponse) => {
+                console.log("getDecriptedUserProfile error ", err);
+            }
+        )
+        return this.lmsData;
+    }
+     // LMS part end
 
 }
