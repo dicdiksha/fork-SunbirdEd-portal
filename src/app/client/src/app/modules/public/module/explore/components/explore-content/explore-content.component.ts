e0cf2d067d9d0089165892c973643e0d166e2406
TejaswiniGiram19 <144098064+TejaswiniGiram19@users.noreply.github.com>
2024-07-25 16:25:08 +0530

    228855-Fixed issue for NCERT content search (#366)

--- src/app/client/src/app/modules/public/module/explore/components/explore-content/explore-content.component.ts
+++ src/app/client/src/app/modules/public/module/explore/components/explore-content/explore-content.component.ts
@@ -312,7 +312,10 @@ export class ExploreContentComponent implements OnInit, OnDestroy, AfterViewInit
     if( this.queryParams.selectedTab==="all" && _.toLower(option.filters.se_boards) === 'ncert' ){
       option.filters.visibility = option.filters.visibility.filter(item => item !== "Parent");
     }
-    
+    // To empty se_boards when NCERT is selected as NCERT board is not bind to any content
+    if(option.filters.se_boards && option.filters.se_boards.length>0 && _.toLower(option.filters.se_boards).includes('ncert')){
+      option.filters.se_boards = [];
+    }
     this.searchService.contentSearch(option)
       .pipe(
         mergeMap(data => {
