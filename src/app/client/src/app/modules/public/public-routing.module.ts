f5e0d33d21a3fe34a6f3a50fdedf6e3125cfba2f
yashveertrigyn <yashveer.negi@trigyn.com>
2024-07-31 13:42:02 +0530

    remove exploren commented code

--- src/app/client/src/app/modules/public/public-routing.module.ts
+++ src/app/client/src/app/modules/public/public-routing.module.ts
@@ -15,9 +15,9 @@ const routes: Routes = [
   {
     path: 'explore', loadChildren: () => import('./module/explore/explore.module').then(m => m.ExploreModule)
   },
-  // {
-  //   path: 'exploren', loadChildren: () => import('./module/explore-new/explore-new.module').then(m => m.ExploreNewModule)
-  // },
+  {
+    path: 'exploren', loadChildren: () => import('./module/explore-new/explore-new.module').then(m => m.ExploreNewModule)
+  },
   {
     path: 'explore-course', loadChildren: () => import('./module/course/course.module').then(m => m.CourseModule)
   },
