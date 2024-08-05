0381a3b6e592ea21e8582ad04193f7336a520092
yashveertrigyn <yashveer.negi@trigyn.com>
2024-08-05 10:31:30 +0530

    195124-update method to patch

--- src/app/client/src/app/modules/public/module/chat-with-books/components/chat-with-books.component.ts
+++ src/app/client/src/app/modules/public/module/chat-with-books/components/chat-with-books.component.ts
@@ -297,7 +297,7 @@ export class ChatWithBooksComponent implements OnInit, OnDestroy, AfterViewInit 
       url: this.configService.urlConFig.URLS.CHAT_WITH_BOOKS.UPDATE,
       data: {
         "request": {
-          "id": this.userService?.userid ?? '4af1c209-5055-4d5b-9d4a-a72cde6b0bc6',
+          "id": this.userService?.userid,
           "userId": questionId,
           "searchQuery": searchQuery.trim(),
           "searchQueryResponse": searchQueryResponse.trim(),
@@ -306,7 +306,7 @@ export class ChatWithBooksComponent implements OnInit, OnDestroy, AfterViewInit 
       }
     }
 
-    this.learnerService.postWithSubscribe(option).subscribe((res: any) => {
+    this.learnerService.saveFeedback(option).subscribe((res: any) => {
       if (res.responseCode !== 'OK') {
         //no action
         console.log("res====",res)
